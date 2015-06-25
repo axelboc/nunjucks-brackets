7b344a8676db8b8f5d94e2214965fe0daab54852
axelboc <axelboc@gmail.com>
2015-06-25 22:06:56 +1000

    simple mode configuration

--- main.js
+++ main.js
@@ -11,40 +11,57 @@ define(function (require, exports, module) {
 	], function (CodeMirror) {
 		
 		// Define a new CodeMirror simple mode 
-		CodeMirror.defineSimpleMode("nunjucks", {
+		CodeMirror.defineSimpleMode("njk", {
 			start: [
-				{ regex: /\{\{/, push: "expression", token: "tag" },
+				{ regex: /{% raw %}/, token: "tag", mode: { spec: "htmlmixed", end: /{% endraw %}/ } },
+				{ regex: /\{\{/, push: "block", token: "tag" },
 				{ regex: /\{\%\-?/, push: "block", token: "tag" },
 				{ regex: /\{\#/, push: "comment", token: "comment" }
 			],
-			expression: [
-				{ regex: /}}/, pop: true, token: "tag" }
-			],
 			block: [
+				{ regex: /}}/, pop: true, token: "tag" },
 				{ regex: /\-?\%\}/, pop: true, token: "tag" },
 
 				// Double and single quotes
 				{ regex: /"(?:[^\\]|\\.)*?"/, token: "string" },
 				{ regex: /'(?:[^\\]|\\.)*?'/, token: "string" },
 
-				// Handlebars keywords
-//				{ regex: />|[#\/]([A-Za-z_]\w*)/, token: "keyword" },
-//				{ regex: /(?:else|this)\b/, token: "keyword" },
+				// Keywords
+				{ regex: /\b(?:block|if|for|macro|call|raw)\b/, token: "keyword" },
+				{ regex: /\b(?:endblock|endif|endfor|endmacro|endcall|endraw)\b/, token: "keyword" },
+				{ regex: /\b(?:extends|include|elif|else|import|set)\b/, token: "keyword" },
+				{ regex: /\b(?:asyncEach|endeach|asyncAll|endall)\b/, token: "keyword" },
+				{ regex: /\b(?:and|or|not|in|as|from)\b/, token: "keyword" },
+				
+				// Atoms
+				{ regex: /\b(?:true|false)\b/, token: "atom" },
 
 				// Numeral
-				{ regex: /\d+/i, token: "number" },
+				{ regex: /\b\d+(?:\.\d+)?\b/, token: "number" },
 
-				// Atoms like = and .
-//				{ regex: /=|~|@|true|false/, token: "atom" },
-
-				// Paths
-//				{ regex: /(?:\.\.\/)*(?:[A-Za-z_][\w\.]*)+/, token: "variable-2" }
+				// Operators
+				{ regex: /(?:\=|\+|\-|\/|\*|%|\!|<|>)/, token: "operator" },
+				
+				// Functions and filters
+				{ regex: /\w+(?=\()/, token: "def" },
+				{ regex: /(\|)(\w+)/, token: ["operator", "def"] },
+				
+				// Variable
+				{ regex: /\w+/, token: "variable" }
 			],
 			comment: [
 				{ regex: /\#\}/, pop: true, token: "comment" },
 				{ regex: /./, token: "comment" }
 			]
 
+		});
+		
+		// Overlay the new mode with CodeMirror's HTML mode
+		CodeMirror.defineMode("nunjucks", function(config, parserConfig) {
+			return CodeMirror.overlayMode(
+				CodeMirror.getMode(config, "htmlmixed"),
+				CodeMirror.getMode(config, "njk")
+			);
 		});
 
 		// Let Brackets know about the new language
